8b07f87194b408ae7f5ef8713fbaeb64f48f4ee0
Thien Chau <tchau006@ucr.edu>
2018-08-30 21:34:31 -0700

    Sessions 8/31

--- 2D PLATFORMER/Assets/session_3_josh_noble.cs
+++ 2D PLATFORMER/Assets/session_3_josh_noble.cs
@@ -1,20 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System; //Math Library
 
 public class session_3_josh_noble : MonoBehaviour {
     
     //Functions, Parameters, and Return Values
+    public int add_two_numbers(int x, int y) //Function add_two_numbers. Parameters x and y.
+    {
+        return x + y; //Return value: x + y
+    }
+
+    public double pyth_thm(double a, double b) //Function pyth thm on two numbers.
+    {
+        return Math.Sqrt(a * a + b * b); //Return value: c = sqrt(a^2+b^2)
+    }
 
     //Global Variables/Constants
+    public const double GLOBAL_PI = 3.14159265359;
 
-    //Text Files
-    
-    //Exception Handling
+    //Text Files. Output to Text File. Read from Text File to string
+
+    //Exception Handling. Try/Catch/Throw Statements
 
     // Use this for initialization
-	void Start () {
-		
+    void Start () {
+        Debug.Log(add_two_numbers(1, 2)); //Outputs 3 to console
+        Debug.Log(pyth_thm(3, 4)); //Outputs 5 to console
+        Debug.Log(GLOBAL_PI);
+        //GLOBAL_PI = 5; //Outputs error since trying to modify constant value
 	}
 	
 	// Update is called once per frame
--- 2D PLATFORMER/Assets/session_4_josh_noble.cs
+++ 2D PLATFORMER/Assets/session_4_josh_noble.cs
@@ -1,16 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class session_4_josh_noble : MonoBehaviour {
-    
-    //What is a class? What is an object
-    
-    //"self" and "init" -> Class variables and constructors
 
+    //What is a class? What is an object
+    //In the Unity editor, whenever we create a script we create a new class.
+    //We can create objects of these classes in other scripts. (create session_3_josh_noble object)
+    public session_3_josh_noble obj; //Create an object of "session_3...". Note: Important to name scripts well
+    public session_3_josh_noble obj2; //**If script with session3_... is enabled, Start() is executed and displayed
+
+    //"self" and "init" -> Class variables and constructors
+    public class fruits
+    {
+        public int num_in_fruit_basket = 15; //Amt of fruits in basket
+
+        public void disp_fruit_basket_num()
+        {
+            int num_in_fruit_basket = 25;
+            Debug.Log(num_in_fruit_basket); //displays 25
+            Debug.Log(this.num_in_fruit_basket); //"this." is the equivalent of "self". Used to access class variable
+        }
+    }
+
+    public class pyth_thm
+    {
+        double x, y;
+        public pyth_thm()//Constructor setting x = 0, y = 0
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public pyth_thm(double x, double y) //Constructor setting x and y values
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void calc_hypot()
+        {
+            Debug.Log(Math.Sqrt(x * x + y * y));
+        }
+    }
+    public pyth_thm obj4 = new pyth_thm(); //Create object using constructor with no values
+    public pyth_thm obj5 = new pyth_thm(3, 4); //Create object using constructor with values
+    public fruits obj3 = new fruits();
     // Use this for initialization
-	void Start () {
-		
+    void Start () {
+        Debug.Log(obj.add_two_numbers(5, 6)); //Will display 11
+        //Debug.Log(obj.GLOBAL_PI);
+        obj3.disp_fruit_basket_num();
+        obj4.calc_hypot();
+        obj5.calc_hypot();
 	}
 	
 	// Update is called once per frame
